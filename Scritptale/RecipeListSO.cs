304118c06f0beca48954dab7facc82f6d3be4fa1
anna1577623417 <129138975+anna1577623417@users.noreply.github.com>
2024-06-05 17:08:18 +0800

    Add files via upload

---
+++ Scritptale/RecipeListSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[CreateAssetMenu] 创建后就注释掉，保证只有一个RecipeListSO
+public class RecipeListSO : ScriptableObject
+{
+    public List<RecipeSO> recipeSOList;
+
+}
